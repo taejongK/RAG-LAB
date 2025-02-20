906cbc7eff0071bb3bbc1a379756d3ab2f9d0178
taejong.kim <taegjong.kim@brique.co.kr>
2025-02-20 14:19:33 +0900

    service and client

--- app/service.py
+++ app/service.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from repository import ChatbotRepository
 from datetime import datetime
+from chatbot import *
 
 uuid2room: Dict[str, str] = {}
 
@@ -17,20 +18,26 @@ class SampleCreateResponseService:
     '''
     나중에 LLM model로 변경 예정
     '''
+
     def __init__(self):
-        self.repo = ChatbotRepository() # 대화 내용 저장소 인스턴스 생성
+        self.repo = ChatbotRepository()  # 대화 내용 저장소 인스턴스 생성
 
     def create_response(self, request: dict) -> dict:
         uuid = request.uuid
         question = request.query
-        question_timestamp = int(datetime.now().timestamp()) # 질문 시간, 정확히는 질문이 넘어온 시간
+        # 질문 시간, 정확히는 질문이 넘어온 시간
+        question_timestamp = int(datetime.now().timestamp())
 
-        if question == "안녕":
-            answer = "안녕하세요"
-            response_timestamp = int(datetime.now().timestamp()) # 응답 시간
-        else:
-            answer = "죄송합니다. 이해할 수 없습니다."
-            response_timestamp = int(datetime.now().timestamp()) # 응답 시간
+        # if question == "안녕":
+        #     answer = "안녕하세요"
+        #     response_timestamp = int(datetime.now().timestamp()) # 응답 시간
+        # else:
+        #     answer = "죄송합니다. 이해할 수 없습니다."
+        #     response_timestamp = int(datetime.now().timestamp()) # 응답 시간
+        answer = chain_with_history.invoke({"question": question},
+                                           config={"session_id": uuid})
+        # answer = self.chatbot.get_response(question, uuid)
+        response_timestamp = int(datetime.now().timestamp())
 
         # 저장 코드
         self.repo.save_conversation({
@@ -40,11 +47,6 @@ class SampleCreateResponseService:
             "question_timestamp": question_timestamp,
             "response_timestamp": response_timestamp
         })
-        
-        history = self.repo.get_history()
-        print('history: ', len(history))
-        for chat in history:
-            print(f"ID: {chat.id}, UUID: {chat.uuid}, 질문: {chat.question}, 응답: {chat.response}")
 
         return {
             "uuid": uuid,
